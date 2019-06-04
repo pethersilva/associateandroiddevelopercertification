0a99d36328145d3f7b0dc09a47c8145d012ae291
Pether Jose da Silva <pether@gmail.com>
2019-06-03 23:24:13 -0300

    adding job schedule service

---
+++ CertificationAndroidCoreJobScheduler/app/src/main/java/pethersilva/com/certificationandroidcorejobscheduler/services/NotificationJobService.kt
@@ -0,0 +1,61 @@
+package pethersilva.com.certificationandroidcorejobscheduler.services
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.job.JobParameters
+import android.app.job.JobService
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import pethersilva.com.certificationandroidcorejobscheduler.MainActivity
+import pethersilva.com.certificationandroidcorejobscheduler.R
+
+class NotificationJobService : JobService() {
+
+    private var mNotificationManager: NotificationManager? = null
+    private val PRIMARY_CHANNEL_ID = "primary_notification_channel"
+    private val NOTIFICATION_ID = 0
+
+    override fun onStartJob(p0: JobParameters?): Boolean {
+        createNotificationChannel()
+
+        val pendingIntent = PendingIntent.getActivity(this, 0, Intent(this, MainActivity::class.java),
+            PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val notification = NotificationCompat.Builder(this, PRIMARY_CHANNEL_ID)
+            .setContentTitle("Job Service")
+            .setContentText("Your job ran to completion")
+            .setContentIntent(pendingIntent)
+            .setSmallIcon(R.drawable.ic_job_running)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .setAutoCancel(true)
+        mNotificationManager?.notify(NOTIFICATION_ID, notification.build())
+        return false
+    }
+
+    override fun onStopJob(p0: JobParameters?): Boolean {
+        return true
+    }
+
+    private fun createNotificationChannel() {
+        mNotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            val notificationChannel = NotificationChannel(
+                PRIMARY_CHANNEL_ID, "Notification Channel",
+                NotificationManager.IMPORTANCE_HIGH
+            )
+
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.RED
+            notificationChannel.enableVibration(true)
+            notificationChannel.description = "Notification Description"
+            mNotificationManager?.createNotificationChannel(notificationChannel)
+        }
+    }
+}
