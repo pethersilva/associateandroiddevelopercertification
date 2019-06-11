95f18d80f203c8cb4be0f1f165d5203e76734127
Pether Silva <pether@gmail.com>
2019-06-11 17:02:51 -0300

    adding a log into doInBackground method

--- code_challenge/jobschedulercodechallenge/app/src/main/java/com/pethersilva/jobschedulercodechallenge/services/AsyncTaskJobService.kt
+++ code_challenge/jobschedulercodechallenge/app/src/main/java/com/pethersilva/jobschedulercodechallenge/services/AsyncTaskJobService.kt
@@ -3,11 +3,13 @@ package com.pethersilva.jobschedulercodechallenge.services
 import android.app.job.JobParameters
 import android.app.job.JobService
 import android.os.AsyncTask
+import android.util.Log
 
 class AsyncTaskJobService: JobService() {
 
     companion object {
         var jobParameters: JobParameters? = null
+        val TAG = "AsyncTaskJobService"
     }
 
     override fun onStopJob(params: JobParameters?): Boolean {
@@ -31,8 +33,9 @@ class AsyncTaskSleep : AsyncTask<JobService, Void, Void>() {
 
         jobService = params[0]
 
-        for (x in 0 until 10) {
+        for (interaction in 0 until 10) {
             Thread.sleep(1000)
+            Log.d(AsyncTaskJobService.TAG, "interaction: $interaction")
         }
         return null
     }
