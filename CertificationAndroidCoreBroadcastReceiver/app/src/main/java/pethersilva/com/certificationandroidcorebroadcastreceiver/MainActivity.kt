a66a5376b173eb5971ecbbc1eeb023934ac908e3
Pether Silva <pether@gmail.com>
2019-08-07 09:51:31 -0300

    adding plug and unplugged power broadcast receiver

--- CertificationAndroidCoreBroadcastReceiver/app/src/main/java/pethersilva/com/certificationandroidcorebroadcastreceiver/MainActivity.kt
+++ CertificationAndroidCoreBroadcastReceiver/app/src/main/java/pethersilva/com/certificationandroidcorebroadcastreceiver/MainActivity.kt
@@ -1,12 +1,33 @@
 package pethersilva.com.certificationandroidcorebroadcastreceiver
 
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.buttonSendBroadcast
+import pethersilva.com.certificationandroidcorebroadcastreceiver.receivers.CustomReceiver
 
 class MainActivity : AppCompatActivity() {
+
+    private val mReceiver : CustomReceiver = CustomReceiver()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intentFilter : IntentFilter = IntentFilter()
+        intentFilter.addAction(Intent.ACTION_POWER_CONNECTED)
+        intentFilter.addAction(Intent.ACTION_POWER_DISCONNECTED)
+
+        registerReceiver(mReceiver, intentFilter)
+
+        buttonSendBroadcast.setOnClickListener {
+
+        }
+    }
+
+    override fun onDestroy() {
+        unregisterReceiver(mReceiver)
+        super.onDestroy()
     }
 }
